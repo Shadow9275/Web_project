3fdc22bc87288e2174bb41ddfdb27460e49f159f
Shadow9275 <86415268+Shadow9275@users.noreply.github.com>
2024-12-15 18:13:19 -0500

    correc

---
+++ vue-basic-app/src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="bg-dark text-white text-center p-3">
+      <p>© 2024 My Vue App. All rights reserved.</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppFooter',
+  };
+  </script>
+  
+  <style scoped>
+  footer {
+    position: relative;
+    bottom: 0;
+    width: 100%;
+  }
+  </style>
+  
---
+++ vue-basic-app/src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+      <div v-for="product in products" :key="product.id" class="col-md-4">
+        <div class="card">
+          <img :src="product.image" :alt="product.name" class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">{{ product.price }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        products: [
+          { id: 1, name: "Product A", price: "$20", image: "/path/to/image1.jpg" },
+          { id: 2, name: "Product B", price: "$30", image: "/path/to/image2.jpg" },
+          { id: 3, name: "Product C", price: "$40", image: "/path/to/image3.jpg" },
+        ],
+      };
+    },
+  };
+  </script>
+  
